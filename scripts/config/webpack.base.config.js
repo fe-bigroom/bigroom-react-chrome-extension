7b532e3f5a739ff9dbb10ce78a83b311384e0b7d
lzwai <kevinlzwai@gmail.com>
2018-09-21 17:05:41 +0800

    修复脚手架抽取配置 bug

--- scripts/config/webpack.base.config.js
+++ scripts/config/webpack.base.config.js
@@ -79,17 +79,12 @@ module.exports = {
       name: true,
       cacheGroups: {
         commons: {
-					chunks: 'initial',
+          name: 'vendor',
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'all',
 					minChunks: 2,
 					maxInitialRequests: 5, // The default limit is too small to showcase the effect
 					minSize: 0 // This is example is too small to create commons chunks
-				},
-				vendor: {
-					test: /node_modules/,
-					chunks: 'initial',
-					name: 'vendor',
-					priority: 10,
-					enforce: true
 				}
       }
     }
